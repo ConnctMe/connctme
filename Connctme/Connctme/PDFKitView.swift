07fc9734ebc01488c387781a61a000117e4ae2f7
Gareth <garethm773@gmail.com>
2020-10-10 15:02:48 -0400

    PDFViewer

---
+++ Connctme/Connctme/PDFKitView.swift
@@ -0,0 +1,37 @@
+//
+//  PDFKitView.swift
+//  Connctme
+//
+//  Created by Gareth Mansfield on 10/10/20.
+//
+
+import Foundation
+import SwiftUI
+import PDFKit
+
+import PDFKit
+
+struct PDFKitView: View {
+    var url: URL
+    var body: some View {
+        PDFKitRepresentedView(url)
+    }
+}
+
+struct PDFKitRepresentedView: UIViewRepresentable {
+    let url: URL
+    init(_ url: URL) {
+        self.url = url
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<PDFKitRepresentedView>) -> PDFKitRepresentedView.UIViewType {
+        let pdfView = PDFView()
+        pdfView.document = PDFDocument(url: self.url)
+        pdfView.autoScales = true
+        return pdfView
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<PDFKitRepresentedView>) {
+        // Update the view.
+    }
+}
