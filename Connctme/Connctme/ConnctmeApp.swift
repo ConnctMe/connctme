decb1adc77f3828116049f93dcee7f060f1b3746
Emily Y <emilygbs@yahoo.com>
2020-10-10 12:06:46 -0400

    added Firebase

--- Connctme/Connctme/ConnctmeApp.swift
+++ Connctme/Connctme/ConnctmeApp.swift
@@ -6,15 +6,40 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ConnctmeApp: App {
     let persistenceController = PersistenceController.shared
-
-    var body: some Scene {
+    
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+      var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+          ContentView()
+            .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+      }
+    }
+
+    class AppDelegate: NSObject, UIApplicationDelegate {
+      func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+      }
+    }
+
+//    let persistenceController = PersistenceController.shared
+//
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//        }
+//    }
+//}
+
+struct ConnctmeApp_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
     }
 }
