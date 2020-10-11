14b081bcb6d4cdddffc30112a81409640864b748
EKKOING <nicholas.lorentzen@gmail.com>
2020-10-10 21:29:22 -0400

    Add Nav Bar Icons

--- Connctme/Connctme/ContentView.swift
+++ Connctme/Connctme/ContentView.swift
@@ -16,17 +16,20 @@ struct ContentView: View {
             ConnectionsScreen(connections: [
                 Connection(name: "Nicolas Lorentzen", image: ProfileImage( imageFile: "nlorentzen"), description: "Full Stack Developer @ Google", tags: [TagData(text: "Google", color: .red), TagData(text: "Recruiter", color: .yellow), TagData(text: "Boston, MA", color: .green), TagData(text: "Internship", color: .blue)], meetingTime: "Tuesday", meetingEvent: "Google Job Fair", meetingLocation: "Providence, RI"),
                 Connection(name: "Nicolas Lorentzen", image: ProfileImage(imageFile: "nlorentzen"), description: "Full Stack Developer @ Google", tags: [TagData(text: "Google", color: .red), TagData(text: "Google", color: .red)], meetingTime: "Tuesday", meetingEvent: "Google Career Fair", meetingLocation: "Providence, RI"),
-                Connection(name: "Nicolas Lorentzen", image: ProfileImage(imageFile: "nlorentzen"), description: "Full Stack Developer @ Google", tags: [TagData(text: "Google", color: .red), TagData(text: "Google", color: .red)], meetingTime: "Tuesday", meetingEvent: "Google Career Fair", meetingLocation: "Providence, RI")])
+                Connection(name: "Nicholas Lorentzen", image: ProfileImage(imageFile: "nlorentzen"), description: "Full Stack Developer @ Google", tags: [TagData(text: "Google", color: .red), TagData(text: "Google", color: .red)], meetingTime: "Tuesday", meetingEvent: "Google Career Fair", meetingLocation: "Providence, RI")])
                 .tabItem {
                     Text("My Connections")
+                    Image(systemName: "list.bullet")
                 }.tag(1)
             AddConnectionScreen()
                 .tabItem {
                     Text("Connect")
+                    Image(systemName: "plus.circle.fill")
                 }.tag(2)
-            ProfileScreen(profile: Profile(name: "Nicolas Lorentzen", image: ProfileImage(size: 100, imageFile: "nlorentzen"), description: "Full Stack Dev at Google", location: "Providence, RI", tags: [TagData(text: "Google", color: .purple), TagData(text: "Providence", color: .red)], about: "hsafgkdjhgsdkjf sadgfsadh weuiohqw hdkasfdasf  sasf sdfga ewy asdfug se fyasgkdf ashdf wf yausgdfasydf weyfgdasuyfgsa waefygs dauyfgas  wefy agsdfgsd f", resume: URL(string: "https://writing.colostate.edu/guides/documents/resume/functionalSample.pdf")))
+            ProfileScreen(profile: Profile(name: "Nicholas Lorentzen", image: ProfileImage(size: 100, imageFile: "nlorentzen"), description: "Full Stack Dev at Google", location: "Providence, RI", tags: [TagData(text: "Google", color: .purple), TagData(text: "Providence", color: .red)], about: "hsafgkdjhgsdkjf sadgfsadh weuiohqw hdkasfdasf  sasf sdfga ewy asdfug se fyasgkdf ashdf wf yausgdfasydf weyfgdasuyfgsa waefygs dauyfgas  wefy agsdfgsd f", resume: URL(string: "https://writing.colostate.edu/guides/documents/resume/functionalSample.pdf")))
                 .tabItem {
                     Text("My Profile")
+                    Image(systemName: "person.crop.circle.fill")
                 }.tag(3)
         }
     }
